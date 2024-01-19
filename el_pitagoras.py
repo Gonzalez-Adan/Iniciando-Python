5fd344511ff9c78975acaae1130ca7bad83efd9d
Adan Gonzalez <gonzalezadan486@gmail.com>
2024-01-19 19:39:00 -0300

    practicando con python

---
+++ el_pitagoras.py
@@ -0,0 +1,9 @@
+import math
+
+print("Hola, desde loma")
+# Tipado dinamico
+# Aplicar el teorema de pitagoras
+catetoA = 2;
+catetoB = 2;
+hipo = math.sqrt( pow(catetoA, 2) + pow(catetoB, 2))
+print(f"La hipotenusa es {hipo}")
---
+++ los_parseos.py
@@ -0,0 +1,13 @@
+nombre = "Adan"
+apellido = "Gonzalez"
+edad = 18
+peso = 72.5
+
+print(type(nombre))
+print(type(edad))
+print(type(peso))
+
+# Intentar el parsing
+edad_en_string = str(edad)
+print(f"La edad anterior era {type(edad)}") 
+
